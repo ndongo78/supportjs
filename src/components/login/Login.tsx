53e1dc4753613e02bbab413a5c14c67826c7c726
ndongo78 <ndongodahs@yahoo.fr>
2023-04-03 13:29:25 +0200

    bug fixed

--- src/components/login/Login.tsx
+++ src/components/login/Login.tsx
@@ -3,7 +3,7 @@ import { LockOutlined, UserOutlined } from '@ant-design/icons';
 import { Button, Checkbox, Form, Input } from 'antd';
 import logo from "../../assets/logo.png"
 
-const Login: React.FC = ({loginUser}) => {
+const Login: React.FC = ({loginUser}:any) => {
   const onFinish = (values: any) => {
     loginUser(values)
    //console.log('Received values of form: ', values);
